96784208c8e39fc2e02748c4111ea328ce9feaa5
Redhouane <redouane.sobaihi@live.com>
2021-09-25 14:48:38 +0700

    feat(): add tests and fixes

--- src/charts/charts.controller.spec.ts
+++ src/charts/charts.controller.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ChartsController } from './charts.controller'
+import { ChartsService } from './charts.service'
 
 describe('ChartsController', () => {
   let controller: ChartsController
 
   beforeEach(async () => {
+    /* eslint-disable prettier/prettier */
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ChartsController]
-    }).compile()
+      controllers: [ChartsController],
+      providers: [ChartsService]
+    })
+      .overrideProvider(ChartsService)
+      .useValue({
+        getDataByTicker: () => ({
+          entries: []
+        })
+      })
+      .compile()
 
     controller = module.get<ChartsController>(ChartsController)
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  it('should return a value', async () => {
+    const result = await controller.candlestick('TSLA', 2019)
+
+    expect(result).toHaveProperty('entries')
+    expect(result.entries).toStrictEqual([])
   })
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
-import { AppModule } from './../src/app.module'
+import { ChartsService } from '../src/charts/charts.service'
+import { AppModule } from '../src/app.module'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
+  const chartServiceMock = {
+    getDataByTicker: () => ({
+      entries: []
+    })
+  }
 
   beforeEach(async () => {
+    /* eslint-disable prettier/prettier */
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule]
-    }).compile()
+    })
+      .overrideProvider(ChartsService)
+      .useValue(chartServiceMock)
+      .overrideProvider('Database')
+      .useValue({})
+      .compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
   })
 
-  it('/ (GET)', () => {
+  it('GET /ticker/year', () => {
+    const url = '/charts/acme/2020'
     return request(app.getHttpServer())
-      .get('/')
+      .get(url)
       .expect(200)
-      .expect('Hello World!')
+      .expect({ entries: [] })
+  })
+
+  it('GET /ticker/year (incorrect year)', () => {
+    const badUrl = '/charts/acme/AAAA'
+    return request(app.getHttpServer()).get(badUrl).expect(400)
+  })
+
+  afterAll(async () => {
+    await app.close()
   })
 })
